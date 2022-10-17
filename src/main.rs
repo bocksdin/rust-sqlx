65d2016b54029e04e1fab7d8915918abcd2fc2a4
Rory Letteney <rory.letteney@gmail.com>
2022-10-17 11:57:53 -0500

    Recorded video

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web::Data, App, HttpServer};
 use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 mod services;
 use services::{create_user_article, fetch_user_articles, fetch_users};
 
+pub struct AppState {
+    db: Pool<Postgres>
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Error building a connection pool");
 
     HttpServer::new(move || {
         App::new()
+            .app_data(Data::new(AppState { db: pool.clone() }))
             .service(fetch_users)
             .service(fetch_user_articles)
             .service(create_user_article)
--- src/services.rs
+++ src/services.rs
@@ -1,9 +1,26 @@
 use actix_web::{
     get, post,
-    web::{Json, Path},
-    Responder,
+    web::{Data, Json, Path},
+    Responder, HttpResponse
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use sqlx::{self, FromRow};
+use crate::AppState;
+
+#[derive(Serialize, FromRow)]
+struct User {
+    id: i32,
+    first_name: String,
+    last_name: String,
+}
+
+#[derive(Serialize, FromRow)]
+struct Article {
+    id: i32,
+    title: String,
+    content: String,
+    created_by: i32,
+}
 
 #[derive(Deserialize)]
 pub struct CreateArticleBody {
@@ -12,18 +29,50 @@ pub struct CreateArticleBody {
 }
 
 #[get("/users")]
-pub async fn fetch_users() -> impl Responder {
-    "GET /users".to_string()
+pub async fn fetch_users(state: Data<AppState>) -> impl Responder {
+    // "GET /users".to_string()
+
+    match sqlx::query_as::<_, User>("SELECT id, first_name, last_name FROM users")
+        .fetch_all(&state.db)
+        .await
+    {
+        Ok(users) => HttpResponse::Ok().json(users),
+        Err(_) => HttpResponse::NotFound().json("No users found"),
+    }
 }
 
 #[get("/users/{id}/articles")]
-pub async fn fetch_user_articles(path: Path<i32>) -> impl Responder {
+pub async fn fetch_user_articles(state: Data<AppState>, path: Path<i32>) -> impl Responder {
     let id: i32 = path.into_inner();
-    format!("GET /users/{id}/articles")
+    // format!("GET /users/{id}/articles")
+
+    match sqlx::query_as::<_, Article>(
+        "SELECT id, title, content, created_by FROM articles WHERE created_by = $1"
+    )
+        .bind(id)
+        .fetch_all(&state.db)
+        .await
+    {
+        Ok(articles) => HttpResponse::Ok().json(articles),
+        Err(_) => HttpResponse::NotFound().json("No articles found"),
+    }
 }
 
 #[post("/users/{id}/articles")]
-pub async fn create_user_article(path: Path<i32>, body: Json<CreateArticleBody>) -> impl Responder {
+pub async fn create_user_article(state: Data<AppState>, path: Path<i32>, body: Json<CreateArticleBody>) -> impl Responder {
     let id: i32 = path.into_inner();
-    format!("POST /users/{id}/articles")
+    // format!("POST /users/{id}/articles")
+
+    match sqlx::query_as::<_, Article>(
+        "INSERT INTO articles (title, content, created_by) VALUES ($1, $2, $3) RETURNING id, title, content, created_by"
+    )
+        .bind(body.title.to_string())
+        .bind(body.content.to_string())
+        .bind(id)
+        .fetch_one(&state.db)
+        .await
+    {
+        Ok(article) => HttpResponse::Ok().json(article),
+        Err(_) => HttpResponse::InternalServerError().json("Failed to create user article"),
+    }
 }
